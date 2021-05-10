a7166ec8385891f0025054d734bdf9e8cf4e596f
work pc <357160383@qq.com>
2021-05-10 16:37:32 +0800

    创建游戏项目

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    '''游戏主类，用于管理整个游戏'''
+
+    def __init__(self):
+        ''' 初始化游戏并创建资源 '''
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        ''' 游戏循环 '''
+        while True:
+            #监测输入事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #绘制屏幕
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ''' 利用主类创建实例并运行 '''
+    ai = AlienInvasion()
+    ai.run_game()
