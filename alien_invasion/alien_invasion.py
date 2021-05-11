419f42a755ce34b3dbb5c88be89aab7526dbf946
Coudy <357160383@qq.com>
2021-05-11 23:10:53 +0800

    完善飞船功能

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -13,9 +13,15 @@ class AlienInvasion:
 
         #创建设置类对象
         self.settings = Settings()
+
+        #窗口
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width,self.settings.screen_height))
-        
+        #全屏
+#        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+#        self.settings.screen_width = self.screen.get_rect().width
+#        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         #创建飞船对象
@@ -24,15 +30,58 @@ class AlienInvasion:
     def run_game(self):
         ''' 游戏循环 '''
         while True:
-            #监测输入事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #填充屏幕颜色
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()#调用飞船绘制方法绘制飞船
-            #绘制屏幕
-            pygame.display.flip()
+            #调用输入事件方法
+            self._check_events()
+            #更新飞船位置
+            self.ship.update()
+            #调用刷新画面方法
+            self._update_screen()
+
+    def _check_events(self):
+        ''' 监测输入事件 '''
+        for event in pygame.event.get():
+            #监测到点击窗口X按钮后关闭窗口
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            #监测键盘事件
+            elif event.type == pygame.KEYDOWN:  #监测按下
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:    #监测松开
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self,event):
+        ''' 监测按下事件 '''
+        if event.key == pygame.K_RIGHT:
+            #开启飞船对象右移状态
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            #开启左移状态
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            #按q关闭程序
+            sys.exit()
+
+    def _check_keyup_events(self,event):
+        ''' 监测松开事件 '''
+        if event.key == pygame.K_RIGHT:
+            #关闭右移状态
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+                    #关闭左移状态
+            self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        ''' 负责画面刷新的方法 '''
+        #填充屏幕颜色
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()#调用飞船绘制方法绘制飞船
+
+        #绘制屏幕
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ''' 利用主类创建实例并运行 '''
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -5,8 +5,9 @@ class Ship:
     def __init__(self,ai_game):
         ''' 初始化飞船并且设置位置 '''
 
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen = ai_game.screen                    #获取朱磊屏幕对象
+        self.settings = ai_game.settings                #获取主类设置对象的属性
+        self.screen_rect = ai_game.screen.get_rect()    #获取主类屏幕大小
 
         #加载图片和获取图片长宽
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,25 @@ class Ship:
         
         #初始化飞船位置，放在屏幕中间
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #飞船X值转化为float
+        self.x = float(self.rect.x)
+
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        ''' 飞船状态更新 '''
+
+        #改变飞船X的值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        #根据floatX的值，调整飞船屏幕位置，飞船rect对象只能接受整数
+        self.rect.x = self.x
 
     def blitme(self):
         ''' 绘制飞船 '''
