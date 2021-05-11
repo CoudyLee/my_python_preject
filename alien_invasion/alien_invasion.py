344b28b11be360e0780d061c1b8878e340441766
work pc <357160383@qq.com>
2021-05-11 16:32:48 +0800

    游戏项目创建飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,15 +1,26 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     ''' 游戏主类，用于管理整个游戏 '''
 
     def __init__(self):
         ''' 初始化游戏并创建资源 '''
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+
+        #创建设置类对象
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
+        
         pygame.display.set_caption("Alien Invasion")
 
+        #创建飞船对象
+        self.ship = Ship(self)
+        
     def run_game(self):
         ''' 游戏循环 '''
         while True:
@@ -17,6 +28,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            #填充屏幕颜色
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()#调用飞船绘制方法绘制飞船
             #绘制屏幕
             pygame.display.flip()
 
---
+++ alien_invasion/ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship:
+    ''' 飞船类 '''
+    def __init__(self,ai_game):
+        ''' 初始化飞船并且设置位置 '''
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #加载图片和获取图片长宽
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        #初始化飞船位置，放在屏幕中间
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        ''' 绘制飞船 '''
+        self.screen.blit(self.image,self.rect)
